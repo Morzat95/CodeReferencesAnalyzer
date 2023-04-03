ffa1203ec5bd4e9a72098d7ef63a71d3d7cd8d6b
Federico Nicolás Quioga <fe_de_qui9@hotmail.com>
2023-04-03 00:55:16 -0300

    Update algorithm to print the line number of the method reference instead of the method line number

--- src/main/java/com/example/flowtrackerplugin/action/PopupDialogAction.java
+++ src/main/java/com/example/flowtrackerplugin/action/PopupDialogAction.java
@@ -7,12 +7,19 @@ import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.ListIterator;
+import java.util.Stack;
+import static java.util.AbstractMap.SimpleEntry;
 
 import static com.intellij.icons.AllIcons.Actions.DiagramDiff;
 
@@ -28,12 +35,12 @@ public class PopupDialogAction extends AnAction {
 
         // Search for references to the method
         if (psiElement instanceof PsiMethod) {
-            analyzeReferences(event.getProject(), (PsiMethod) psiElement, new Stack<>());
+            analyzeReferences(event.getProject(), (PsiMethod) psiElement, null, new Stack<>());
         }
     }
 
-    private void analyzeReferences(Project project, PsiMethod method, Stack<PsiMethod> visitedMethods) {
-        visitedMethods.push(method);
+    private void analyzeReferences(Project project, PsiMethod method, PsiElement methodReference, Stack<SimpleEntry<PsiMethod, PsiElement>> visitedMethods) {
+        visitedMethods.push(new SimpleEntry<>(method, methodReference));
 
         // Search for references to the method
         Collection<PsiReference> references = ReferencesSearch.search(method).findAll();
@@ -44,7 +51,7 @@ public class PopupDialogAction extends AnAction {
             // Get the parent method for the current reference
             PsiMethod parentMethod = PsiTreeUtil.getParentOfType(referenceElement, PsiMethod.class);
 
-            analyzeReferences(project, parentMethod, visitedMethods);
+            analyzeReferences(project, parentMethod, referenceElement, visitedMethods);
         }
 
         if (references.isEmpty()) {
@@ -54,10 +61,10 @@ public class PopupDialogAction extends AnAction {
         visitedMethods.pop();
     }
 
-    private void printStack(Project project, Stack<PsiMethod> visitedMethods) {
+    private void printStack(Project project, Stack<SimpleEntry<PsiMethod, PsiElement>> visitedMethods) {
         int indentation = 0;
 
-        ListIterator<PsiMethod> listIterator = visitedMethods.listIterator(visitedMethods.size());
+        ListIterator<SimpleEntry<PsiMethod, PsiElement>> listIterator = visitedMethods.listIterator(visitedMethods.size());
 
         while (listIterator.hasPrevious()) {
             printMessage(project, listIterator.previous(), indentation);
@@ -65,19 +72,26 @@ public class PopupDialogAction extends AnAction {
         }
     }
 
-    private void printMessage(Project project, PsiMethod method, int indentation) {
+    private void printMessage(Project project, SimpleEntry<PsiMethod, PsiElement> entry, int indentation) {
+        PsiMethod method = entry.getKey();
+
         // Get class and method names
         String methodName = method.getName();
         String className = method.getContainingClass().getName();
 
         // Calculate line number of method definition
-        PsiFile file = method.getContainingFile();
-        Document document = PsiDocumentManager.getInstance(project).getDocument(file);
-        int lineNumber = document.getLineNumber(method.getTextRange().getStartOffset()) + 1;
+        PsiElement methodReference = entry.getValue();
+        int lineNumber = calculateLineNumber(project, methodReference == null ? method : methodReference);
 
         // Print the result into console
         String padding = new String(new char[indentation]).replace('\0', ' '); // I have to do this because the %0s format breaks
         System.out.printf("%s%s.%s:%d%n", padding, className, methodName, lineNumber);
+    }
+
+    private int calculateLineNumber(Project project, PsiElement element) {
+        PsiFile file = element.getContainingFile();
+        Document document = PsiDocumentManager.getInstance(project).getDocument(file);
+        return document.getLineNumber(element.getTextRange().getStartOffset()) + 1;
     }
 
     @Override
