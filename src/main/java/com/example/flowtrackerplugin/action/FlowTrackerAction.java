070e5fa7dd0976ffae6430f674e8d0b971c93339
Federico Nicolás Quioga <fe_de_qui9@hotmail.com>
2023-04-10 00:44:59 -0300

    Uptade algorithm to ignore tests

--- src/main/java/com/example/flowtrackerplugin/action/FlowTrackerAction.java
+++ src/main/java/com/example/flowtrackerplugin/action/FlowTrackerAction.java
@@ -7,6 +7,7 @@ import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -39,11 +40,29 @@ public class FlowTrackerAction extends AnAction {
         }
     }
 
-    private void analyzeReferences(Project project, PsiMethod method, PsiElement methodReference, Stack<SimpleEntry<PsiMethod, PsiElement>> visitedMethods) {
+    /**
+     * The goal of the algorithm is to analyze a Java codebase and identify all the methods that are called exclusively from non-test methods, i.e., methods that are not called by test code.
+     * To accomplish this, the algorithm traverses the call hierarchy of each non-test method in the codebase and identifies all the methods that it calls. If a called method is not a test
+     * method itself and is only called by test methods or is not called by any other method, it is considered a leaf node in the call hierarchy. The algorithm then prints out the full call
+     * stack from the leaf node back to the original non-test method. This allows developers to identify and potentially refactor methods that are only used by non-test code, which may help
+     * to improve code maintainability and reduce unnecessary code complexity.
+     * @param project
+     * @param method
+     * @param methodReference
+     * @param visitedMethods
+     * @return
+     */
+    private boolean analyzeReferences(Project project, PsiMethod method, PsiElement methodReference, Stack<SimpleEntry<PsiMethod, PsiElement>> visitedMethods) {
+
+        if (isTestClass(method.getContainingClass()) || isTestMethod(method)) {
+            return true;
+        }
+
         visitedMethods.push(new SimpleEntry<>(method, methodReference));
 
         // Search for references to the method
         Collection<PsiReference> references = ReferencesSearch.search(method).findAll();
+        boolean isFinalNode = true;
 
         for (PsiReference reference : references) {
             PsiElement referenceElement = reference.getElement();
@@ -51,14 +70,31 @@ public class FlowTrackerAction extends AnAction {
             // Get the parent method for the current reference
             PsiMethod parentMethod = PsiTreeUtil.getParentOfType(referenceElement, PsiMethod.class);
 
-            analyzeReferences(project, parentMethod, referenceElement, visitedMethods);
+            boolean parentIsTestNode = analyzeReferences(project, parentMethod, referenceElement, visitedMethods);
+
+            isFinalNode = isFinalNode && parentIsTestNode;
         }
 
-        if (references.isEmpty()) {
+        // Print the complete path
+        if (isFinalNode && visitedMethods.size() > 1) {
             printStack(project, visitedMethods);
         }
 
+        // Go back one level
         visitedMethods.pop();
+        return false;
+    }
+
+    private boolean isTestClass(PsiClass psiClass) {
+        return isTestDefinition(psiClass.getName());
+    }
+
+    private boolean isTestMethod(PsiMethod psiMethod) {
+        return isTestDefinition(psiMethod.getName());
+    }
+
+    private boolean isTestDefinition(String definition) {
+        return definition.toLowerCase().endsWith("test");
     }
 
     private void printStack(Project project, Stack<SimpleEntry<PsiMethod, PsiElement>> visitedMethods) {
